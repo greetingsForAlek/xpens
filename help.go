055e085cda1ab16c90822a762b9c205f42c472a1
greetingsForAlek <alexpwoods@outlook.com>
2026-09-07 19:36:07 +0100

    Check command for mis-spellings

--- help.go
+++ help.go
@@ -17,12 +17,33 @@ func checkCommand(command string) {
 	fmt.Println(letters)
 
 	if len(letters) == 3 {
-		fmt.Println("3 letters")
+		if letters[0] == "l" || letters[1] == "o" || letters[2] == "g" {
+			fmt.Printf("Unrecognised command '%s'. Similar command: log\n", command)
+			fmt.Println("For a list of all commands, run 'xpens help'.")
+		} else {
+			fmt.Printf("Unrecognised command '%s'.\n", command)
+			fmt.Println("For a list of all commands, run 'xpens help'.")
+		}
 	} else if len(letters) == 4 {
-		fmt.Println("4 letters")
+		if letters[0] == "h" || letters[1] == "e" || letters[2] == "l" || letters[3] == "p" {
+			fmt.Printf("Unrecognised command '%s'. Similar command: help. Run help for a list of commands.\n", command)
+		} else {
+			fmt.Printf("Unrecognised command '%s'.\n", command)
+			fmt.Println("For a list of all commands, run 'xpens help'.")
+		}
 	} else if len(letters) == 6 {
-		fmt.Println("6 letters")
+		if letters[0] == "r" || letters[1] == "e" || letters[2] == "c" || letters[3] == "a" || letters[4] == "l" || letters[5] == "l" {
+			fmt.Printf("Unrecognised command '%s'. Similar command: recall\n", command)
+			fmt.Println("For a list of all commands, run 'xpens help'.")
+		} else if letters[0] == "c" || letters[1] == "r" || letters[2] == "u" || letters[3] == "n" || letters[4] == "c" || letters[5] == "h" {
+			fmt.Printf("Unrecognised command '%s'. Similar command: crunch\n", command)
+			fmt.Println("For a list of all commands run 'xpens help'.")
+		} else {
+			fmt.Printf("Unrecognised command '%s'.\n", command)
+			fmt.Println("For a list of all commands, run 'xpens help'.")
+		}
 	} else {
-		fmt.Println("Amount of letters does not match any command")
+		fmt.Printf("Unrecognised command '%s'.\n", command)
+		fmt.Println("For a list of all commands, run 'xpens help'.")
 	}
 }
